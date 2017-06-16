981c6d175e3f289295baa8983df12e50a40f384f
Tim Garcia <tim.m.garcia@gmail.com>
2017-06-16 10:28:36 -0700

    Fixed spacing on react auth example

--- Unit-02/examples/redux-auth-client-server/redux-auth/src/Welcome.js
+++ Unit-02/examples/redux-auth-client-server/redux-auth/src/Welcome.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const Welcome = (props) => (
-      <div>
-            Welcome! You are logged in!
-      </div>
-    )
+  <div>
+    Welcome! You are logged in!
+  </div>
+);
 
 export default Welcome
